be71235e0f80204ee06e7de5d8650029b735e152
Kishan <kishangupta21995@gmail.com>
2019-05-16 16:48:02 -0400

    hit save beyont grocery

--- app/src/main/java/marketplace/jpr/com/allapigroferss/Adapter/CategoryDashboardAdapter.java
+++ app/src/main/java/marketplace/jpr/com/allapigroferss/Adapter/CategoryDashboardAdapter.java
@@ -1,30 +1,83 @@
-/*
 package marketplace.jpr.com.allapigroferss.Adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import marketplace.jpr.com.allapigroferss.R;
+import marketplace.jpr.com.allapigroferss.models.dashboard.BlockbusterSaver;
+import marketplace.jpr.com.allapigroferss.models.dashboard.Category;
 
 public class CategoryDashboardAdapter extends RecyclerView.Adapter<CategoryDashboardAdapter.ViewHolder> {
-    private List<>categoryDashboardAdapter;
+    private List<Category>categoryDashboardAdapter;
+    Context context;
+
+    //click listner
+    private SellingListInterface sellingListInterface;
+
+    public CategoryDashboardAdapter(Context context) {
+        this.context = context;
+    }  ////
+
+
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View itemView = LayoutInflater.from(viewGroup.getContext())   ////
+                .inflate(R.layout.recycler_categorydashboardadapter, viewGroup, false);   ////
+
+        return new CategoryDashboardAdapter.ViewHolder(itemView);
     }
+
+    public void setdata(List<Category> itemList) {
+        this.categoryDashboardAdapter = itemList;
+
+    }  ////
+
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
+
+        final Category alItems = categoryDashboardAdapter.get(i);
+        viewHolder.getCatTitle.setText(alItems.getCatTitle());
+        viewHolder.getCatDesc.setText(alItems.getCatDesc());
+        viewHolder.getDTitle.setText(alItems.getDTitle());
+
+        Picasso.with(context).load(alItems.getCatImage())
+                .error(R.drawable.veg).into(viewHolder.getCatImage);
+
+        //click listner
+        viewHolder.getCatImage.setOnClickListener(new View.OnClickListener() {   //
+            @Override
+            public void onClick(View view) {                                     //
+                if (sellingListInterface != null) {                              //
+                    sellingListInterface.sellinglistitem(alItems.getCatId());    //
+                }                                                                //
+
+            }                                                                    //
+        });                                                                      //
+
+
+
     }
+
+    //click listner
+    public void setSellingListInterface(SellingListInterface sellingListInterface) {   //
+        this.sellingListInterface = sellingListInterface;                             //
+    }                                                                                //
+
+
 
     @Override
     public int getItemCount() {
@@ -33,18 +86,24 @@ public class CategoryDashboardAdapter extends RecyclerView.Adapter<CategoryDashb
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView itemImage16;
-        private TextView textView13, textView14, textView15, textView4;
+        private ImageView getCatImage;
+        private TextView getCatTitle, getCatDesc, getDTitle;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            textView13 = itemView.findViewById(R.id.text_tv11);
-            textView14 = itemView.findViewById(R.id.text_tv11);
-            textView15 = itemView.findViewById(R.id.text_tv11);
-            itemImage16= itemView.findViewById(R.id.logoID1);
+            getCatTitle = itemView.findViewById(R.id.text_tv11);
+            getCatDesc= itemView.findViewById(R.id.text_tv11);
+            getDTitle = itemView.findViewById(R.id.text_tv11);
+            getCatImage= itemView.findViewById(R.id.logoID1);
 
         }
     }
+
+
+
+    //click listner
+    public interface SellingListInterface {         //
+        public void sellinglistitem(String id);    //
+    }                                             //
+
 }
-*/
