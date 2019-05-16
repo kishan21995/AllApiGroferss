5ee9f881a7a2e64d796130878e70d22a3eae9212
Kishan <kishangupta21995@gmail.com>
2019-05-16 16:58:58 -0400

    hit save beyont grocery

---
+++ app/src/main/java/marketplace/jpr/com/allapigroferss/Activities/SubCatrgoryActivity.java
@@ -0,0 +1,15 @@
+package marketplace.jpr.com.allapigroferss.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import marketplace.jpr.com.allapigroferss.R;
+
+public class SubCatrgoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_catrgory);
+    }
+}
